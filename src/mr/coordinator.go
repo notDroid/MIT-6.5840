f9d03a55505b26e8c9d55ae48e22b743edba79d2
Wali Mirza <127229451+notDroid@users.noreply.github.com>
2025-03-31 00:09:12 -0400

    Started lab 1.

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,33 +1,129 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
-
+import (
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"sync"
+)
 
 type Coordinator struct {
-	// Your definitions here.
+	// Tracking and assigning map tasks
+	M          int              // total number of map tasks to be processed
+	nMap       int              // number of active map tasks
+	mapFiles   []string         // list of map input file names
+	mapSocks   map[int]string   // Map from map worker ids to their sockets
+	mapIdsLeft map[int]struct{} // Map ids left set
 
+	// Tracking completed map tasks
+	mapIFiles map[int][]string // map results, intermediate file names
+
+	// Tracking and assigning reduce tasks
+	R             int              // total number of reduce tasks
+	nReduce       int              // number of active reduce tasks
+	reduceIds     map[int]string   // Map from reduce worker ids to their sockets
+	reduceIdsLeft map[int]struct{} // Reduce ids left set
 }
 
-// Your code here -- RPC handlers for the worker to call.
+var cm = sync.Mutex{}
 
-//
-// an example RPC handler.
-//
+// Your code here -- RPC handlers for the worker to call.
 // the RPC argument and reply types are defined in rpc.go.
-//
-func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
-	reply.Y = args.X + 1
+
+// Request task from coordinater
+func (c *Coordinator) GetTask(args *Identifier, reply *TaskReply) error {
+	cm.Lock()
+	defer cm.Unlock()
+
+	// If map tasks remaining assign map
+	if c.nMap > 0 {
+		// Assign Map
+		c.nMap -= 1
+		reply.task = "map"
+		reply.mapTask.R = c.R
+
+		// Grab a remaining map id
+		var mId int
+		for key := range c.mapIdsLeft {
+			mId = key
+			break
+		}
+		c.mapSocks[mId] = args.sock
+
+		// Get and send filename
+		filename := c.mapFiles[c.nMap]
+		reply.mapTask.filename = filename
+
+		// Read file contents
+		file, err := os.Open(filename)
+		if err != nil {
+			log.Fatalf("cannot open %v", filename)
+		}
+		contents, err := io.ReadAll(file)
+		file.Close()
+		if err != nil {
+			log.Fatalf("cannot read %v", filename)
+		}
+
+		// Send file contents
+		reply.mapTask.contents = string(contents)
+
+	} else if c.nReduce < c.R {
+		// Assign reduce task
+		c.nReduce += 1
+		reply.task = "reduce"
+		reply.reduceTask.M = c.M
+
+		// Grab a remaining reduce id
+		var rId int
+		for key := range c.reduceIdsLeft {
+			rId = key
+			break
+		}
+		reply.reduceTask.id = rId
+		c.reduceIds[rId] = args.sock
+
+		// Send map worker locations
+		for mId, filenames := range c.mapIFiles {
+			iFile := IFile{
+				sock:     c.mapSocks[mId],
+				filename: filenames[mId],
+			}
+			reply.reduceTask.iFiles[mId] = iFile
+		}
+	}
+
 	return nil
 }
 
+// Map task must report locations of files on local disk, so that reduce f
+func (c *Coordinator) ReportCompletedTask(args *MapIntermediate, reply *struct{}) error {
+	cm.Lock()
+	defer cm.Unlock()
 
-//
+	// Get file locations
+	c.mapIFiles[args.id] = args.iFiles
+
+	// Broadcast locations to reduce workers
+	for rId, rSock := range c.reduceIds {
+		ok := call(rSock, "ReduceWorker.SendMapIntermediate", args.iFiles[rId], struct{}{})
+
+		// If the reduce worker doesn't respond, assume its dead
+		if !ok {
+			// Remove the reduce task from the active set and add it to the reduce tasks left set
+			c.nReduce -= 1
+			delete(c.reduceIds, rId)
+			c.reduceIdsLeft[rId] = struct{}{}
+		}
+	}
+
+	return nil
+}
+
 // start a thread that listens for RPCs from worker.go
-//
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -41,29 +137,19 @@ func (c *Coordinator) server() {
 	go http.Serve(l, nil)
 }
 
-//
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
-//
 func (c *Coordinator) Done() bool {
 	ret := false
-
-	// Your code here.
-
 
 	return ret
 }
 
-//
 // create a Coordinator.
 // main/mrcoordinator.go calls this function.
 // nReduce is the number of reduce tasks to use.
-//
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	c := Coordinator{}
-
-	// Your code here.
-
 
 	c.server()
 	return &c
--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -6,24 +6,53 @@ package mr
 // remember to capitalize all names.
 //
 
-import "os"
-import "strconv"
+import (
+	"fmt"
+	"log"
+	"net/rpc"
+	"os"
+	"strconv"
+)
 
 //
 // example to show how to declare the arguments
 // and reply for an RPC.
 //
 
-type ExampleArgs struct {
-	X int
+type Identifier struct {
+	sock string // Could be (ip, port) if we have multiple computers
 }
 
-type ExampleReply struct {
-	Y int
+type TaskReply struct {
+	task       string
+	mapTask    MapTask
+	reduceTask ReduceTask
+}
+
+type MapTask struct {
+	R        int
+	id       int
+	filename string
+	contents string
+}
+
+type ReduceTask struct {
+	M      int
+	id     int
+	iFiles map[int]IFile
+}
+
+type MapIntermediate struct {
+	id     int
+	sock   string // Could be (ip, port) if we have multiple computers
+	iFiles []string
+}
+
+func main() {
+
 }
 
 // Add your RPC definitions here.
-
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
@@ -33,4 +62,38 @@ func coordinatorSock() string {
 	s := "/var/tmp/5840-mr-"
 	s += strconv.Itoa(os.Getuid())
 	return s
+}
+
+func mapSock(id int) string {
+	s := "/var/tmp/5840-mr-"
+	s += strconv.Itoa(os.Getuid())
+	s += "-m-" + strconv.Itoa(id) // Map id part
+	return s
+}
+
+func reduceSock(id int) string {
+	s := "/var/tmp/5840-mr-"
+	s += strconv.Itoa(os.Getuid())
+	s += "-r-" + strconv.Itoa(id) // Reduce id part
+	return s
+}
+
+// send an RPC request, wait for the response.
+// returns false if something goes wrong.
+func call(sockname string, rpcname string, args interface{}, reply interface{}) bool {
+	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
+	c, err := rpc.DialHTTP("unix", sockname)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	defer c.Close()
+
+	err = c.Call(rpcname, args, reply)
+	if err == nil {
+		return true
+	}
+
+	fmt.Println(err)
+	return false
 }
